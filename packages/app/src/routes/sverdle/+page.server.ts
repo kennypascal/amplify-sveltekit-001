3ac0fc09523277566566a47bf83afeb38d689a49
KP <kp@kennypascal.com>
2022-10-04 10:58:33 -0400

    Testing removal of prerender

--- packages/app/src/routes/sverdle/+page.server.ts
+++ packages/app/src/routes/sverdle/+page.server.ts
@@ -8,7 +8,25 @@ import type { PageServerLoad, Actions } from './$types';
 Amplify.configure({ ...awsExports, ssr: true });
 
 export const load: PageServerLoad = ({ cookies }) => {
-	return null
+	const game = new Game(cookies.get('sverdle'));
+
+	return {
+		/**
+		 * The player's guessed words so far
+		 */
+		guesses: game.guesses,
+
+		/**
+		 * An array of strings like '__x_c' corresponding to the guesses, where 'x' means
+		 * an exact match, and 'c' means a close match (right letter, wrong place)
+		 */
+		answers: game.answers,
+
+		/**
+		 * The correct answer, revealed if the game is over
+		 */
+		answer: game.answers.length >= 6 ? game.answer : null
+	};
 };
 
 export const actions: Actions = {
